e0a9c52b76e90af39de5620117a9886294077e96
kaghog <grace.kagho@ivt.baug.ethz.ch>
2023-10-23 15:47:36 +0200

    solution to week4 exercise

--- src/main/java/abmt2023/week4/exercises/average_time/CalculateAverageTravelTime.java
+++ src/main/java/abmt2023/week4/exercises/average_time/CalculateAverageTravelTime.java
@@ -2,6 +2,7 @@ package abmt2023.week4.exercises.average_time;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.matsim.api.core.v01.Id;
@@ -11,35 +12,57 @@ import org.matsim.api.core.v01.events.handler.PersonArrivalEventHandler;
 import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
 import org.matsim.api.core.v01.population.Person;
 
-public class CalculateAverageTravelTime implements PersonDepartureEventHandler, PersonArrivalEventHandler{
-	
-	//you will need a HashMap<>() to store information
+public class CalculateAverageTravelTime implements PersonDepartureEventHandler, PersonArrivalEventHandler {
+	private final Map<Id<Person>, Double> times = new HashMap<>();
+	private final Map<String, ArrayList<Object>> timesByMode = new HashMap<>(); //first and last list elements are counts and traveltime sum
+	private double travelTimeSum = 0.0;
+	private int travelTimeCount = 0;
 
 	//get the departure time for each person
 	@Override
 	public void handleEvent(PersonDepartureEvent event) {
-		
+		this.times.put(event.getPersonId(), event.getTime());
 		
 	}
 
 	//get the arrival time and calculate the duration
 	@Override
 	public void handleEvent(PersonArrivalEvent event) {
+		double depTime = this.times.get(event.getPersonId());
+		double travelTime = event.getTime() - depTime;
+		String mode = event.getLegMode();
 		
+		if(timesByMode.containsKey(mode)) {
+			ArrayList<Object> tmp = timesByMode.get(mode);
+			timesByMode.get(mode).set(0, (int)tmp.get(0) + 1);
+			timesByMode.get(mode).set(1, (double)tmp.get(1) + travelTime);
+			//System.out.println(timesByMode.get(mode));
+		} else {
+			ArrayList<Object> traveltimeList = new ArrayList<Object>() {{
+	            add(1);
+	            add(travelTime);
+	        }};
+			timesByMode.put(mode, traveltimeList);
+		} 
+		this.travelTimeSum += travelTime;
+		this.travelTimeCount++;
 		 
 	}
 	
 	//return travel time
 	public double getAverageTravelTime() {
-		return 0.0;
+		return this.travelTimeSum / this.travelTimeCount;
 		}
 
 	//return travel time
 	public Map<String, Double> getAverageTravelTimePerMode() {
-		
-		return null;
+		Map<String, Double> avgTimePerMode = new HashMap<>();
+		for (String mode : timesByMode.keySet()) {
+			double avgTime = (double)timesByMode.get(mode).get(1)/(int)timesByMode.get(mode).get(0);
+			avgTimePerMode.put(mode, avgTime);
 		}
-	
+		return avgTimePerMode;
+		}
 
 
 
