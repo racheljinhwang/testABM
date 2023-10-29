741b9a714104151c1a1e76e5ca1497a90438b0d0
balacmi <milos.balac@ivt.baug.ethz.ch>
2023-10-29 09:48:38 +0100

    Adding example classes for the injection lecture

---
+++ src/main/java/abmt2023/week7/lectures/Link.java
@@ -0,0 +1,15 @@
+package abmt2023.week7.lectures;
+
+public class Link {
+
+	private final String name;
+
+	public Link(String name) {
+
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
---
+++ src/main/java/abmt2023/week7/lectures/Network.java
@@ -0,0 +1,14 @@
+package abmt2023.week7.lectures;
+
+public class Network {
+	
+	private Link link;
+	
+	public Network(Link link) {
+		this.link = link;
+	}
+	
+	public Link getLink() {
+		return this.link;
+	}
+}
